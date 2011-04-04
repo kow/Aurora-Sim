ffe94d90aaffa3f034e6f956170bdd5b3a0776f9
Patrick Sapinski <patrick@sapinski.com>
2011-04-04 01:03:58 -0400

    package script changes

--- create upgrade archive.bat
+++ create upgrade archive.bat
@@ -10,18 +10,17 @@ copy bin\Aurora.Server.exe.config package
 copy bin\Aurora.32BitLaunch.exe.config package
 copy bin\*.bat package
 copy bin\*.dll package
-copy bin\*.ini package
+::copy bin\*.ini package
 copy bin\*.xml package
 copy bin\*.html package
 copy bin\*.example package
 xcopy bin\ScriptEngines package\ScriptEngines /E /I
-::skip this for now because it's huge!
-::xcopy bin\DefaultInventory package\DefaultInventory /E /I
+xcopy bin\DefaultInventory package\DefaultInventory /E /I
 xcopy bin\Physics package\Physics /E /I
 xcopy bin\Data package\Data /E /I
-xcopy bin\Configuration package\Configuration /E /I
-xcopy bin\AuroraServerConfiguration package\AuroraServerConfiguration /E /I
-del upgrade.7z /Q
+::xcopy bin\Configuration package\Configuration /E /I
+::xcopy bin\AuroraServerConfiguration package\AuroraServerConfiguration /E /I
+::del upgrade*.7z /Q
 cd package
 "C:\Program Files\7-Zip\7z.exe" a -t7z ..\upgrade_%date:~-4,4%-%date:~-7,2%-%date:~-10,2%_%time:~-11,2%-%time:~-8,2%.7z *
 pause
